061e8ffb3dae802825ddc966ad408e6877e8fdfe
Fotis <149146139+Fotis-Toumanides@users.noreply.github.com>
2024-09-13 19:05:36 +0300

    Update settings.py

--- fantasybooks/settings.py
+++ fantasybooks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'covers/'  #For collected static files
 STATICFILES_DIRS = [
-    BASE_DIR / "static/",
+    os.path.join(BASE_DIR, "static/"),
 ]
 
 # Default primary key field type
